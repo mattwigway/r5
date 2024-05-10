c1944dc17c096d51b97fb6bd0511f385a8891a9f
Matt Bhagat-Conway <matt@indicatrix.org>
2024-05-10 11:10:16 -0400

    fix(fares) compile errors

--- src/main/java/com/conveyal/r5/analyst/fare/OldBostonInRoutingFareCalculator.java
+++ src/main/java/com/conveyal/r5/analyst/fare/OldBostonInRoutingFareCalculator.java
@@ -69,7 +69,7 @@ public class OldBostonInRoutingFareCalculator extends InRoutingFareCalculator {
             "place-dwnxg", "place-pktrm"))));
 
     // Logging to facilitate debugging
-    private static final Logger LOG = LoggerFactory.getLogger(BostonInRoutingFareCalculator.class);
+    private static final Logger LOG = LoggerFactory.getLoggerOldBostonInRoutingFareCalculator.class);
 
     private MersenneTwister logRandomizer = LOG_FARES ? new MersenneTwister() : null;
 
@@ -244,7 +244,7 @@ public class OldBostonInRoutingFareCalculator extends InRoutingFareCalculator {
                 if (fares == null){
                     synchronized (fareSystemCache) {
                         FareSystemWrapper fareSystem = fareSystemCache.computeIfAbsent(this.transitLayer,
-                                BostonInRoutingFareCalculator::loadFaresFromGTFS);
+                               OldBostonInRoutingFareCalculator::loadFaresFromGTFS);
                         this.fares = fareSystem.fares;
                         this.fares.defaultFare = DEFAULT_FARE_ID;
                     }
